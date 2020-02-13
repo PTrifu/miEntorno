b28b41153a1e52cf527d3763dc5701606f7e83f4
PTrifu <cwmf_94@hotmail.com>
2020-02-13 19:12:58 +0100

    pula2

--- data/laravel/database/seeds/DatabaseSeeder.php
+++ data/laravel/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Cliente;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // dd(clientesSeeder.php::class);
         // $this->call(UsersTableSeeder::class);
+       //$this->call(clientesSeeder::class);
+        factory(Cliente::class,10)->create();
+
     }
 }
